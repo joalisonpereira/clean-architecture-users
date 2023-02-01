acd634ee4103844d543ee4690e08169e4bff4ebf
joalisonpereira <joalison.pereira@hotmail.com>
2023-02-01 10:00:20 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
+  }
+};
---
+++ tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src", "!src/**/*.spec.*", "!src/tests/**"],
+  splitting: false,
+  sourcemap: false,
+  clean: false,
+});
